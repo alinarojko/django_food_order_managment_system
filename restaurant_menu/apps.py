658d880194cae611142ccb501f3e28eec881afdd
ArthurProgrammer1 <rojko.alina@gmail.com>
2025-04-30 10:36:31 +0100

    Create Class-Based view for both pages

--- restaurant_menu/apps.py
+++ restaurant_menu/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class RestourantMenuConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'restourant_menu'
+    name = 'restaurant_menu'
